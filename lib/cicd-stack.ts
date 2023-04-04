12f4de26b2d08f99b08af98704f54a9727b8eb8e
tayyab <123@gmail.com>
2023-04-04 19:12:12 +0500

    cleaning

--- lib/cicd-stack.ts
+++ lib/cicd-stack.ts
@@ -2,37 +2,7 @@ import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import { CodePipeline, CodePipelineSource, ShellStep, Step } from 'aws-cdk-lib/pipelines';
 import { ManualApprovalStep } from 'aws-cdk-lib/pipelines';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
 
-// export class CicdStack extends cdk.Stack {
-//   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
-//     super(scope, id, props);
-
-//     // The code that defines your stack goes here
-
-//     // example resource
-//     // const queue = new sqs.Queue(this, 'CicdQueue', {
-//     //   visibilityTimeout: cdk.Duration.seconds(300)
-//     // });
-//   }
-// }
-// export class CiCdAwsPipelineDemoStack extends cdk.Stack {
-//   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
-//     super(scope, id, props);
-
-//     const pipeline = new CodePipeline(this, 'Pipeline', {
-//       pipelineName: 'TestPipeline',
-//       synth: new ShellStep('Synth', {
-//         input: CodePipelineSource.gitHub('tayyab-java/ci-cd-aws-pipeline-demo', 'main'), //Remember to change 
-//         commands: ['npm ci', 
-//                    'npm run build', 
-//                    'npx cdk synth']
-//       })
-//     });
-
-
-//   }
-// }
 export class CicdStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
